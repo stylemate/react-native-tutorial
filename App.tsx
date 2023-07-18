d55980377d22beff9c58c01ec02d44854d566f4e
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2023-07-18 16:15:39 +0900

    rn components sucks balls

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { PropsWithChildren } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.upperContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Type things to do" style={styles.textInput} />
+          <View style={styles.addButton}>
+            <Button title="Add" />
+          </View>
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text>List</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  upperContainer: {
+    backgroundColor: '#777',
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 16,
+  },
+  bottomContainer: {
+    backgroundColor: '#dddddd',
+    flex: 4,
+    padding: 16,
+  },
+  inputContainer: {
+    width: 240,
+    flexDirection: 'row',
+    // justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 40,
+  },
+  textInput: {
+    backgroundColor: '#ffffff',
+    padding: 4,
+    paddingVertical: 0,
+    flex: 3,
+    height: '100%',
+  },
+  addButton: {
+    flex: 1,
+    height: '100%',
   },
 });
+
+const CustomText = ({ children }: PropsWithChildren) => (
+  <Text style={{ margin: 20 }}>{children}</Text>
+);
